f0e0b038828da24a56b52ec34e640b1091dd8692
inadram <inadram@gmail.com>
2014-08-05 11:09:24 +0100

    add watcher to look after the css changes automatically

---
+++ src/watch.js
@@ -0,0 +1,22 @@
+var fs = require('fs');
+
+var chokidar = require('chokidar');
+var watcher = chokidar.watch('trunk', {ignored: /[\/\\]\./, persistent: true});
+
+watcher
+  .on('add', function(path) {console.log('File', path, 'has been added');})
+  .on('addDir', function(path) {console.log('Directory', path, 'has been added');})
+  .on('change', function(path) {console.log('File', path, 'has been changed');
+  var stream = fs.createWriteStream("restart.js");
+ stream.once('open', function(fd) {
+  stream.write("filename="+path); 
+  stream.write("\n"); 
+  stream.write("restart="+Math.floor(Math.random()*1000)); 
+  stream.end();
+	})
+  .on('unlink', function(path) {console.log('File', path, 'has been removed');})
+  .on('unlinkDir', function(path) {console.log('Directory', path, 'has been removed');})
+  .on('error', function(error) {console.error('Error happened', error);}) 
+   
+    
+});
